ba0f865d94b3440459d33d22fff4b0c34e10f26e
Kushal Das <kushaldas@gmail.com>
2013-08-21 10:28:32 +0530

    Moves SQL file and fixes tab issue.

--- scripts/addcomponents.py
+++ scripts/addcomponents.py
@@ -7,5 +7,5 @@ f.close()
 
 url = "http://127.0.0.1:9998/component/"
 for comp in comps:
-	d = {'user': 'kushaldas@gmail.com', "password": 'asdf', 'name':comp['name'], 'description': comp['description'], "product_id": 1, "owner_id": 1}
-	r = requests.post(url, data=json.dumps(d))
+    d = {'user': 'kushaldas@gmail.com', "password": 'asdf', 'name':comp['name'], 'description': comp['description'], "product_id": 1, "owner_id": 1}
+    r = requests.post(url, data=json.dumps(d))
