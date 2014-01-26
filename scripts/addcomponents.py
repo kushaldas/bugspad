3ccd35c9efdddb17a119349b964190d48fee7e1f
Kushal Das <kushaldas@gmail.com>
2014-01-27 03:34:18 +0530

    Passing owner email id instead of ID

--- scripts/addcomponents.py
+++ scripts/addcomponents.py
@@ -7,5 +7,5 @@ f.close()
 
 url = "http://127.0.0.1:9998/component/"
 for comp in comps:
-    d = {'user': 'kushaldas@gmail.com', "password": 'asdf', 'name':comp['name'], 'description': comp['description'], "product_id": 1, "owner_id": 1}
+    d = {'user': 'kushaldas@gmail.com', "password": 'asdf', 'name':comp['name'], 'description': comp['description'], "product_id": 1, "owner": "kushaldas@gmail.com"}
     r = requests.post(url, data=json.dumps(d))
